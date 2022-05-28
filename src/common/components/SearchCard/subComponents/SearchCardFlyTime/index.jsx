10fe3de1f6b03de555a14d625d455dff23eb457a
thanhnguyen662 <facebook662311@gmail.com>
2022-05-28 23:38:08 +0700

    fix: 🐛 redirect error at production

--- src/common/components/SearchCard/subComponents/SearchCardFlyTime/index.jsx
+++ src/common/components/SearchCard/subComponents/SearchCardFlyTime/index.jsx
@@ -20,7 +20,7 @@ function SearchCardFlyTime(props) {
             </div>
          </div>
          <div className='flex gap-1 px-6 py-3.5 flex-col leading-none flex-1'>
-            <h2 className='text-lg text-gray-500 font-semibold'>Return</h2>
+            <h2 className='text-lg text-gray-500 font-semibold'>RETURN</h2>
             <div className='flex items-center gap-4 text-purple '>
                <h1 className='font-semibold text-3xl'>Fri, 22 Mar, 2022</h1>
                <BsCalendarWeek className='text-xl' />
